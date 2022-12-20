5009add183ccb354bb3f0b7d530935c4cdcf4eee
Gavin Song <86136270+gavin-St@users.noreply.github.com>
2022-12-19 21:59:53 -0500

    minor refactoring and clean-up

--- openCV_light_detection/brightest_point_detection.py
+++ openCV_light_detection/brightest_point_detection.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # read in the images
-norman = cv2.imread("/Users/petemango/beatSaberProject/openCV_light_detection/norman.png")
-norman_rgb = np.copy(norman)
+img_src = "C:\\Users\\gavin\\OneDrive\Documents\\GitHub\\beatSaberProject\\openCV_light_detection\images\\3.jpg" #i cant get local open to work wtf
+img_main = cv2.imread(img_src)
+img_rgb = np.copy(img_main)
 
 # convert the image to grey scale
-norman_grey = np.copy(norman)
-norman_grey = cv2.cvtColor(norman_grey, cv2.COLOR_BGR2GRAY)
+img_grey = np.copy(img_main)
+img_grey = cv2.cvtColor(img_grey, cv2.COLOR_BGR2GRAY)
 
 # find the max brightness x and y values
-max_x = cv2.minMaxLoc(norman_grey)[3][0]
-max_y = cv2.minMaxLoc(norman_grey)[3][1]
+max_x = cv2.minMaxLoc(img_grey)[3][0]
+max_y = cv2.minMaxLoc(img_grey)[3][1]
 
 # make a square of length 50 to display the brightest spot
 s = 50
@@ -21,7 +22,7 @@ coordinate_right = (max_x + s, max_y + s)
 color = (255, 0, 0)
 t = 5
 
-brightest_box =np.copy(norman_rgb)
+brightest_box =np.copy(img_rgb)
 brightest_box = cv2.rectangle(brightest_box, coordinate_left, coordinate_right, color, t)
 
 # show the plot
