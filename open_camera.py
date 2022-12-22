c073bc276e5b3b69748519eb6b95bf34457f7c61
Daniel Shen <70034924+UncreativeName1@users.noreply.github.com>
2022-12-21 23:16:33 -0500

    Add files via upload

---
+++ open_camera.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+from detect_light import get_brightest_point
+
+# create video capturing
+vid = cv2.VideoCapture(0)
+
+if not vid.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while (True):
+    # Get current frame
+    ret, frame = vid.read()
+    if not ret:
+        print("Stopped receiving stream. Exiting ...")
+        break
+
+    # Call image processing function
+    result = get_brightest_point(frame)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+
+    # press q to exit the video
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# release the video
+vid.release()
+cv2.destroyAllWindows()
